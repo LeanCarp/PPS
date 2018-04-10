87e920d0fb10e5445ac14e648586e9d7be0ce5a7
Leandro <leandrolonardi@gmail.com>
2018-04-10 12:17:28 -0300

    BD

---
+++ bd/ingresofrcu.sql
@@ -0,0 +1,647 @@
+-- phpMyAdmin SQL Dump
+-- version 4.2.11
+-- http://www.phpmyadmin.net
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 10-04-2018 a las 17:15:43
+-- Versión del servidor: 5.6.21
+-- Versión de PHP: 5.6.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de datos: `ingresofrcu`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `actividad`
+--
+
+CREATE TABLE IF NOT EXISTS `actividad` (
+`id` int(11) NOT NULL,
+  `descripcion` text COLLATE utf8_spanish2_ci NOT NULL,
+  `idUsuario` int(10) unsigned NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `archivo`
+--
+
+CREATE TABLE IF NOT EXISTS `archivo` (
+`id` int(11) NOT NULL,
+  `titulo` varchar(150) COLLATE utf8_spanish2_ci NOT NULL,
+  `descripcion` text COLLATE utf8_spanish2_ci,
+  `ruta` varchar(150) COLLATE utf8_spanish2_ci NOT NULL,
+  `idMateria` int(11) NOT NULL,
+  `idCategoriaArchivo` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categoriaarchivo`
+--
+
+CREATE TABLE IF NOT EXISTS `categoriaarchivo` (
+`id` int(11) NOT NULL,
+  `nombre` varchar(80) COLLATE utf8_spanish2_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categoriaforo`
+--
+
+CREATE TABLE IF NOT EXISTS `categoriaforo` (
+`id` int(11) NOT NULL,
+  `nombre` varchar(80) COLLATE utf8_spanish2_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ciudad`
+--
+
+CREATE TABLE IF NOT EXISTS `ciudad` (
+`id` int(11) NOT NULL,
+  `nombre` int(11) NOT NULL,
+  `idPais` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `comision`
+--
+
+CREATE TABLE IF NOT EXISTS `comision` (
+`id` int(11) NOT NULL,
+  `cuatrimestre` varchar(20) COLLATE utf8_spanish2_ci NOT NULL,
+  `anio` int(11) NOT NULL,
+  `idMateria` int(11) NOT NULL,
+  `nombreMateria` varchar(150) COLLATE utf8_spanish2_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cursada`
+--
+
+CREATE TABLE IF NOT EXISTS `cursada` (
+`id` int(11) NOT NULL,
+  `estado` varchar(40) COLLATE utf8_spanish2_ci NOT NULL,
+  `nota` int(11) DEFAULT NULL,
+  `idUsuario` int(11) unsigned NOT NULL,
+  `idComision` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `dicta`
+--
+
+CREATE TABLE IF NOT EXISTS `dicta` (
+`id` int(11) NOT NULL,
+  `idProfesor` int(11) NOT NULL,
+  `idComision` int(11) NOT NULL,
+  `nombreProfesor` varchar(120) COLLATE utf8_spanish2_ci NOT NULL,
+  `rol` varchar(80) COLLATE utf8_spanish2_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `escuela`
+--
+
+CREATE TABLE IF NOT EXISTS `escuela` (
+`id` int(11) NOT NULL,
+  `nombre` varchar(120) COLLATE utf8_spanish2_ci NOT NULL,
+  `orientacion` varchar(80) COLLATE utf8_spanish2_ci NOT NULL,
+  `idCiudad` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `examen`
+--
+
+CREATE TABLE IF NOT EXISTS `examen` (
+`id` int(11) NOT NULL,
+  `idCursada` int(11) NOT NULL,
+  `tipo` varchar(30) COLLATE utf8_spanish2_ci NOT NULL,
+  `descripcion` text COLLATE utf8_spanish2_ci NOT NULL,
+  `nota` int(11) NOT NULL,
+  `comentario` text COLLATE utf8_spanish2_ci
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `groups`
+--
+
+CREATE TABLE IF NOT EXISTS `groups` (
+`id` mediumint(8) unsigned NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `description` varchar(100) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `horario`
+--
+
+CREATE TABLE IF NOT EXISTS `horario` (
+`id` int(11) NOT NULL,
+  `dia` tinyint(4) NOT NULL,
+  `horaInicio` time NOT NULL,
+  `horaFin` time NOT NULL,
+  `idComision` int(11) DEFAULT NULL,
+  `idActividad` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `informe`
+--
+
+CREATE TABLE IF NOT EXISTS `informe` (
+`id` int(11) NOT NULL,
+  `titulo` varchar(80) COLLATE utf8_spanish2_ci NOT NULL,
+  `descripcion` text COLLATE utf8_spanish2_ci NOT NULL,
+  `fecha` date NOT NULL,
+  `idUsuario` int(11) unsigned NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `login_attempts`
+--
+
+CREATE TABLE IF NOT EXISTS `login_attempts` (
+`id` int(11) unsigned NOT NULL,
+  `ip_address` varchar(45) NOT NULL,
+  `login` varchar(100) NOT NULL,
+  `time` int(11) unsigned DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `materia`
+--
+
+CREATE TABLE IF NOT EXISTS `materia` (
+`id` int(11) NOT NULL,
+  `nombre` varchar(150) COLLATE utf8_spanish2_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `mensajeforo`
+--
+
+CREATE TABLE IF NOT EXISTS `mensajeforo` (
+`id` int(11) NOT NULL,
+  `contenido` text COLLATE utf8_spanish2_ci NOT NULL,
+  `posicion` int(11) NOT NULL,
+  `fecha` datetime NOT NULL,
+  `idTema` int(11) NOT NULL,
+  `idUsuario` int(11) unsigned NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pais`
+--
+
+CREATE TABLE IF NOT EXISTS `pais` (
+`id` int(11) NOT NULL,
+  `nombre` varchar(60) COLLATE utf8_spanish2_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `profesor`
+--
+
+CREATE TABLE IF NOT EXISTS `profesor` (
+`id` int(11) NOT NULL,
+  `nombre` varchar(60) COLLATE utf8_spanish2_ci NOT NULL,
+  `apellido` varchar(60) COLLATE utf8_spanish2_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tema`
+--
+
+CREATE TABLE IF NOT EXISTS `tema` (
+`id` int(11) NOT NULL,
+  `titulo` varchar(120) COLLATE utf8_spanish2_ci NOT NULL,
+  `estado` varchar(50) COLLATE utf8_spanish2_ci DEFAULT NULL,
+  `visitas` int(11) DEFAULT NULL,
+  `idCategoriaForo` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+`id` int(11) unsigned NOT NULL,
+  `ip_address` varchar(45) NOT NULL,
+  `username` varchar(100) DEFAULT NULL,
+  `password` varchar(255) NOT NULL,
+  `salt` varchar(255) DEFAULT NULL,
+  `email` varchar(254) NOT NULL,
+  `activation_code` varchar(40) DEFAULT NULL,
+  `forgotten_password_code` varchar(40) DEFAULT NULL,
+  `forgotten_password_time` int(11) unsigned DEFAULT NULL,
+  `remember_code` varchar(40) DEFAULT NULL,
+  `created_on` int(11) unsigned NOT NULL,
+  `last_login` int(11) unsigned DEFAULT NULL,
+  `active` tinyint(1) unsigned DEFAULT NULL,
+  `first_name` varchar(50) DEFAULT NULL,
+  `last_name` varchar(50) DEFAULT NULL,
+  `company` varchar(100) DEFAULT NULL,
+  `phone` varchar(20) DEFAULT NULL,
+  `anioIngreso` int(11) NOT NULL,
+  `carrera` varchar(3) NOT NULL,
+  `idEscuela` int(11) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `users_groups`
+--
+
+CREATE TABLE IF NOT EXISTS `users_groups` (
+`id` int(11) unsigned NOT NULL,
+  `user_id` int(11) unsigned NOT NULL,
+  `group_id` mediumint(8) unsigned NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `actividad`
+--
+ALTER TABLE `actividad`
+ ADD PRIMARY KEY (`id`), ADD KEY `idUsuario` (`idUsuario`);
+
+--
+-- Indices de la tabla `archivo`
+--
+ALTER TABLE `archivo`
+ ADD PRIMARY KEY (`id`), ADD KEY `idMateria` (`idMateria`), ADD KEY `idCategoriaArchivo` (`idCategoriaArchivo`);
+
+--
+-- Indices de la tabla `categoriaarchivo`
+--
+ALTER TABLE `categoriaarchivo`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `categoriaforo`
+--
+ALTER TABLE `categoriaforo`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `ciudad`
+--
+ALTER TABLE `ciudad`
+ ADD PRIMARY KEY (`id`), ADD KEY `idPais` (`idPais`);
+
+--
+-- Indices de la tabla `comision`
+--
+ALTER TABLE `comision`
+ ADD PRIMARY KEY (`id`), ADD KEY `idMateria` (`idMateria`);
+
+--
+-- Indices de la tabla `cursada`
+--
+ALTER TABLE `cursada`
+ ADD PRIMARY KEY (`id`), ADD KEY `idUsuario` (`idUsuario`,`idComision`), ADD KEY `idComision` (`idComision`);
+
+--
+-- Indices de la tabla `dicta`
+--
+ALTER TABLE `dicta`
+ ADD PRIMARY KEY (`id`), ADD KEY `idProfesor` (`idProfesor`), ADD KEY `idComision` (`idComision`);
+
+--
+-- Indices de la tabla `escuela`
+--
+ALTER TABLE `escuela`
+ ADD PRIMARY KEY (`id`), ADD KEY `idCiudad` (`idCiudad`);
+
+--
+-- Indices de la tabla `examen`
+--
+ALTER TABLE `examen`
+ ADD PRIMARY KEY (`id`), ADD KEY `idCursada` (`idCursada`);
+
+--
+-- Indices de la tabla `groups`
+--
+ALTER TABLE `groups`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `horario`
+--
+ALTER TABLE `horario`
+ ADD PRIMARY KEY (`id`), ADD KEY `idComision` (`idComision`), ADD KEY `idActividad` (`idActividad`);
+
+--
+-- Indices de la tabla `informe`
+--
+ALTER TABLE `informe`
+ ADD PRIMARY KEY (`id`), ADD KEY `idUsuario` (`idUsuario`);
+
+--
+-- Indices de la tabla `login_attempts`
+--
+ALTER TABLE `login_attempts`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `materia`
+--
+ALTER TABLE `materia`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `mensajeforo`
+--
+ALTER TABLE `mensajeforo`
+ ADD PRIMARY KEY (`id`), ADD KEY `idTema` (`idTema`), ADD KEY `idUsuario` (`idUsuario`);
+
+--
+-- Indices de la tabla `pais`
+--
+ALTER TABLE `pais`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `profesor`
+--
+ALTER TABLE `profesor`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `tema`
+--
+ALTER TABLE `tema`
+ ADD PRIMARY KEY (`id`), ADD KEY `idCategoriaForo` (`idCategoriaForo`);
+
+--
+-- Indices de la tabla `users`
+--
+ALTER TABLE `users`
+ ADD PRIMARY KEY (`id`), ADD KEY `escuelaId` (`idEscuela`);
+
+--
+-- Indices de la tabla `users_groups`
+--
+ALTER TABLE `users_groups`
+ ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `uc_users_groups` (`user_id`,`group_id`), ADD KEY `fk_users_groups_users1_idx` (`user_id`), ADD KEY `fk_users_groups_groups1_idx` (`group_id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `actividad`
+--
+ALTER TABLE `actividad`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `archivo`
+--
+ALTER TABLE `archivo`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `categoriaarchivo`
+--
+ALTER TABLE `categoriaarchivo`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `categoriaforo`
+--
+ALTER TABLE `categoriaforo`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `ciudad`
+--
+ALTER TABLE `ciudad`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `comision`
+--
+ALTER TABLE `comision`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `cursada`
+--
+ALTER TABLE `cursada`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `dicta`
+--
+ALTER TABLE `dicta`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `escuela`
+--
+ALTER TABLE `escuela`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `examen`
+--
+ALTER TABLE `examen`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `groups`
+--
+ALTER TABLE `groups`
+MODIFY `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT de la tabla `horario`
+--
+ALTER TABLE `horario`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `informe`
+--
+ALTER TABLE `informe`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `login_attempts`
+--
+ALTER TABLE `login_attempts`
+MODIFY `id` int(11) unsigned NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `materia`
+--
+ALTER TABLE `materia`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `mensajeforo`
+--
+ALTER TABLE `mensajeforo`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `pais`
+--
+ALTER TABLE `pais`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `profesor`
+--
+ALTER TABLE `profesor`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `tema`
+--
+ALTER TABLE `tema`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `users`
+--
+ALTER TABLE `users`
+MODIFY `id` int(11) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT de la tabla `users_groups`
+--
+ALTER TABLE `users_groups`
+MODIFY `id` int(11) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `actividad`
+--
+ALTER TABLE `actividad`
+ADD CONSTRAINT `actividad_ibfk_1` FOREIGN KEY (`idUsuario`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `archivo`
+--
+ALTER TABLE `archivo`
+ADD CONSTRAINT `archivo_ibfk_1` FOREIGN KEY (`idMateria`) REFERENCES `materia` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+ADD CONSTRAINT `archivo_ibfk_2` FOREIGN KEY (`idCategoriaArchivo`) REFERENCES `categoriaarchivo` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `ciudad`
+--
+ALTER TABLE `ciudad`
+ADD CONSTRAINT `ciudadId` FOREIGN KEY (`idPais`) REFERENCES `pais` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `comision`
+--
+ALTER TABLE `comision`
+ADD CONSTRAINT `comision_ibfk_1` FOREIGN KEY (`idMateria`) REFERENCES `materia` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `cursada`
+--
+ALTER TABLE `cursada`
+ADD CONSTRAINT `cursada_ibfk_1` FOREIGN KEY (`idUsuario`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+ADD CONSTRAINT `cursada_ibfk_2` FOREIGN KEY (`idComision`) REFERENCES `comision` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `dicta`
+--
+ALTER TABLE `dicta`
+ADD CONSTRAINT `dicta_ibfk_1` FOREIGN KEY (`idProfesor`) REFERENCES `profesor` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+ADD CONSTRAINT `dicta_ibfk_2` FOREIGN KEY (`idComision`) REFERENCES `comision` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `escuela`
+--
+ALTER TABLE `escuela`
+ADD CONSTRAINT `escuelaId` FOREIGN KEY (`idCiudad`) REFERENCES `ciudad` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `examen`
+--
+ALTER TABLE `examen`
+ADD CONSTRAINT `examen_ibfk_1` FOREIGN KEY (`idCursada`) REFERENCES `cursada` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `horario`
+--
+ALTER TABLE `horario`
+ADD CONSTRAINT `horario_ibfk_1` FOREIGN KEY (`idComision`) REFERENCES `comision` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+ADD CONSTRAINT `horario_ibfk_2` FOREIGN KEY (`idActividad`) REFERENCES `actividad` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `informe`
+--
+ALTER TABLE `informe`
+ADD CONSTRAINT `informe_ibfk_1` FOREIGN KEY (`idUsuario`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `mensajeforo`
+--
+ALTER TABLE `mensajeforo`
+ADD CONSTRAINT `mensajeforo_ibfk_1` FOREIGN KEY (`idTema`) REFERENCES `tema` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+ADD CONSTRAINT `mensajeforo_ibfk_2` FOREIGN KEY (`idUsuario`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `tema`
+--
+ALTER TABLE `tema`
+ADD CONSTRAINT `tema_ibfk_1` FOREIGN KEY (`idCategoriaForo`) REFERENCES `categoriaforo` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `users`
+--
+ALTER TABLE `users`
+ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`idEscuela`) REFERENCES `escuela` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `users_groups`
+--
+ALTER TABLE `users_groups`
+ADD CONSTRAINT `fk_users_groups_groups1` FOREIGN KEY (`group_id`) REFERENCES `groups` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+ADD CONSTRAINT `fk_users_groups_users1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
