3ec4e95090635aa5fdada06b0e7614fc6b7a732f
Luciano Thoma Gini <luchothoma@gmail.com>
2018-05-08 14:02:49 -0300

    Refactoring en CapaDeNegocio/Usuario

--- application/libraries/CapaDeNegocio/Usuario.php
+++ application/libraries/CapaDeNegocio/Usuario.php
@@ -25,25 +25,51 @@ class Usuario {
             
     }
 
+    /*
+        //Metodo V 0.1        
+        public function __call($method_name, $args)
+        {
+            //print "Method $method_name called:\n";
+            //var_dump($args);
+
+            if( is_callable([ $this->CI->ion_auth, $method_name]) )
+                if( empty($args) )
+                    return $this->CI->ion_auth->{$method_name}();
+                else
+                    return call_user_func_array( [$this->CI->ion_auth, $method_name], $args);
+            else if( is_callable([ $this->CI->User_model, $method_name]) )
+                if( empty($args) )
+                    return $this->CI->User_model->{$method_name}();
+                else
+                    return call_user_func_array( [$this->CI->User_model, $method_name], $args);
+            else
+                throw new Exception("Metodo ".$method_name." no encontrado en Ion_auth/User_model."); 
+        }
+    */
+   
+   //Metodo V 0.2 
     public function __call($method_name, $args)
     {
-        //print "Method $method_name called:\n";
-        //var_dump($args);
-        //return $this->CI->ion_auth->{$method_name}();
-        if( is_callable([ $this->CI->ion_auth, $method_name]) )
-            if( empty($args) )
-                return $this->CI->ion_auth->{$method_name}();
-            else
-                return call_user_func_array( [$this->CI->ion_auth, $method_name], $args);
-                //$this->CI->ion_auth->{$method_name}($args);
-        else if( is_callable([ $this->CI->User_model, $method_name]) )
-            if( empty($args) )
-                return $this->CI->User_model->{$method_name}();
-            else
-                return call_user_func_array( [$this->CI->User_model, $method_name], $args);
-                //return $this->CI->User_model->{$method_name}($args);
-        else
-            throw new Exception("Metodo ".$method_name." no encontrado en Ion_auth/User_model."); 
+        /****En orden de prioridad de ejecucion.***/
+        //Es decir que si existe una funcion en mas de un objeto,
+        //en el primero que la encuentra por prioridad es donde sera ejecutada.
+        $objectArray = [
+            $this->CI->ion_auth,
+            $this->CI->User_model
+        ];
+
+        foreach ($objectArray as $obj)
+        {
+            if( is_callable([ $obj, $method_name]) )
+                return (
+                    empty($args) ?
+                    $obj->{$method_name}() :
+                    call_user_func_array( [ $obj, $method_name], $args)
+                );
+        }
+        
+        //Si el metodo no fue encontrade y ejecutado en ningun objeto, se lanza una excepsion
+        throw new Exception("Metodo ".$method_name." no encontrado en Ion_auth/User_model."); 
     }
 }
 ?>
